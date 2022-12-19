bc44404c2564dcbd7faf82a07ef97c7d606de8a5
DNS2507 <dhwanilit25@gmail.com>
2022-12-19 21:40:31 +0000

    Java_week6_HW1

--- src/Week6_HW/Circle.java
+++ src/Week6_HW/Circle.java
@@ -2,14 +2,11 @@ package Week6_HW;
 //Write a program to enter any radius value of the circle and find out the
 //area.(Formula of Area A=PI*r*r).
 public class Circle {
-;
-
-
-    public void multiplication(){ //Instance method
+;       public void multiplication(){ //Instance method
         float pi=3.14f; //Instance variable
         int r=5; //Instance variable
         float A = pi*r*r;  //Instance variable
-        System.out.println("Radious of Circle is " +A);
+        System.out.println("Radious of Circle is " +A); //Concetination
 
     }
     public static void main(String[] args) {
